e2f4a4bcab56296e5ed8fffb77f967bc66395907
Brian Malehorn <bmalehorn@gmail.com>
2018-04-25 21:04:08 -0700

    lift setNormal(), add enterNormal() and enterInsert() functions

--- src/bext.ts
+++ src/bext.ts
@@ -7,48 +7,36 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "bext" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('bext.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    context.subscriptions.push(vscode.commands.registerCommand("bext.enterNormal", enterNormal));
+    context.subscriptions.push(vscode.commands.registerCommand("bext.enterInsert", enterInsert));
+    setNormal({ normal: true });
+}
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+function enterNormal() {
+    setNormal({ normal: true });
+}
 
-    context.subscriptions.push(disposable);
+function enterInsert() {
+    setNormal({ normal: false });
+}
 
-    let foo = vscode.commands.registerCommand("bext.foo", () => {
-        vscode.window.showInformationMessage("Foo!");
-    });
-    context.subscriptions.push(foo);
+interface SetNormalArgs {
+    normal: boolean;
+}
 
-    function setNormal(normal: boolean) {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return;
-        }
-        editor.options.cursorStyle = normal ?
-            vscode.TextEditorCursorStyle.Block :
-            vscode.TextEditorCursorStyle.Underline;
-        vscode.commands.executeCommand("setContext", "bext.normal", normal);
-        vscode.commands.executeCommand("setContext", "bext.insert", !normal);
+function setNormal(args: SetNormalArgs) {
+    const { normal } = args;
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
     }
-
-    context.subscriptions.push(vscode.commands.registerCommand("bext.enterNormal", () => {
-        setNormal(true);
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand("bext.enterInsert", () => {
-        setNormal(false);
-    }));
-
-    setNormal(true);
+    editor.options.cursorStyle = normal ?
+        vscode.TextEditorCursorStyle.Block :
+        vscode.TextEditorCursorStyle.Underline;
+    vscode.commands.executeCommand("setContext", "bext.normal", normal);
+    vscode.commands.executeCommand("setContext", "bext.insert", !normal);
 }
 
 // this method is called when your extension is deactivated
