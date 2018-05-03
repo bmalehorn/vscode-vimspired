c7dc885bbc0acd52240b78d4ac94e9ff50318ed4
Brian Malehorn <bmalehorn@gmail.com>
2018-05-02 22:51:06 -0700

    "k" passthrough

--- src/bext.ts
+++ src/bext.ts
@@ -11,7 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(vscode.commands.registerCommand("bext.enterNormal", enterNormal));
     context.subscriptions.push(vscode.commands.registerCommand("bext.enterInsert", enterInsert));
-    setNormal({ normal: true });
+    context.subscriptions.push(vscode.commands.registerCommand("bext.keypressK", keypressK));
+    enterNormal();
 }
 
 function enterNormal() {
@@ -20,6 +21,23 @@ function enterNormal() {
 
 function enterInsert() {
     setNormal({ normal: false });
+}
+
+function keypressK() {
+    console.log("it's happening");
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    editor.edit((editBuilder) => {
+        // during selection there are multiple cursors;
+        // avoid this weirdness
+        if (!editor.selection.isEmpty) {
+            return;
+        }
+        const position = editor.selection.active;
+        editBuilder.insert(position, "k");
+    });
 }
 
 interface SetNormalArgs {
